eb4de28ad965f137900d7533e919ec67eaaa605b
Miriane Silva <mstn@cin.ufpe.br>
2019-05-15 21:50:17 -0300

    Resolução do Passo 2 - Código com AsyncTask, mas ainda não funcional, pois falta adicionar a permissão para acessar internet

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
@@ -1,7 +1,9 @@
 package br.ufpe.cin.if710.rss
 
 import android.app.Activity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Xml
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.textView
@@ -22,42 +24,53 @@ class MainActivity : Activity() {
 
     override fun onStart() {
         super.onStart()
+        var load = LoadXML()
         try {
-            val feedXML = getRssFeed(RSS_FEED)
+            // Executa o asynctask e pega o resultado dessa execução
+            var xmls = load.execute().get()
+
+            // Cria um array com o resultado do asynctask
             val lista = Array(1) {
-                i -> feedXML
+                i -> xmls
             }
+            // Cria um arrayAdapter com a lista criada e popula o list view da main activity
             conteudoRSS.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista)
-        }
-        catch (e: IOException) {
+        } catch (e: IOException) {
             e.printStackTrace()
         }
     }
 
-    @Throws(IOException::class)
-    fun getRssFeed(feed: String): String {
-        var input: InputStream? = null
-        var rssFeed: String
+    internal inner class LoadXML: AsyncTask<Int, Int, String>() {
 
-        try {
-            val url: URL = URL(feed)
-            val conn = url.openConnection() as HttpURLConnection
-            input = conn.inputStream
-            val out = ByteArrayOutputStream()
-            val buffer = ByteArray(1024)
-            var count = input.read(buffer)
-            while (count != -1) {
-                out.write(buffer, 0, count)
-                count = input.read(buffer)
-            }
-            val response = out.toByteArray()
-            rssFeed = String(response, Charsets.UTF_8)
-        } finally {
-            if (input != null) {
-                input.close()
-            }
+        override fun doInBackground(vararg params: Int?): String? {
+            return getRssFeed(RSS_FEED)
         }
 
-        return rssFeed
+        @Throws(IOException::class)
+        fun getRssFeed(feed: String): String {
+            var input: InputStream? = null
+            var rssFeed: String
+
+            try {
+                val url: URL = URL(feed)
+                val conn = url.openConnection() as HttpURLConnection
+                input = conn.inputStream
+                val out = ByteArrayOutputStream()
+                val buffer = ByteArray(1024)
+                var count = input.read(buffer)
+                while (count != -1) {
+                    out.write(buffer, 0, count)
+                    count = input.read(buffer)
+                }
+                val response = out.toByteArray()
+                rssFeed = String(response, Charsets.UTF_8)
+            } finally {
+                if (input != null) {
+                    input.close()
+                }
+            }
+
+            return rssFeed
+        }
     }
 }
